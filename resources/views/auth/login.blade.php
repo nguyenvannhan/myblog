e71feda4f2852cad06c5c508caa5912c61d8b4d1
Van Nhan <nguyenvannhan0810@gmail.com>
2018-09-16 20:51:39 +0700

    Update Login Form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <div class="col-12 col-sm-4 offset-sm-4 d-flex align-items-center">
             <div class="col-12 w-100 bg-white py-3 rounded">
                 <h2 class="text-center py-3">GIA LANG BLOG</h2>
-                <form>
+                <form method="POST" action="{{ route('post_login_route') }}">
                     <div class="form-group">
                         <label for="ip_email">Email: </label>
                         <input type="text" class="form-control" id="ip_email" placeholder="Email" name="email">
@@ -14,6 +14,10 @@
                     <div class="form-group">
                         <label for="ip_password">Password: </label>
                         <input type="text" class="form-control" id="ip_password" placeholder="Password" name="password">
+                    </div>
+                    <div class="form-group text-center">
+                        <button type="submit" class="btn btn-block btn-primary mb-2">Login</button>
+                        <a href="#">Go to home page</a>
                     </div>
                 </form>   
             </div>
