ec6e2b9b8e477c8884612ff288bf2065df38b560
Dao Phuong <daothiphug1997@gmail.com>
2018-09-20 17:32:17 +0700

    Update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,11 +12,12 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            ['role_id'=>"1",
-            'name' => 'Phuong',
-            'email' => 'daothiphuong1997@gmail.com',
-            'password' => bcrypt('matkhau')]
-            
+            ['role_id' => "1", 'name' => 'Phuong', 'email' => 'daothiphuong1997@gmail.com', 'password' => bcrypt('matkhau')],
+            ['role_id' => "1", 'name' => 'Nhan', 'email' => 'NhanNV@gmail.com', 'password' => bcrypt('matkhau')],
+            ['role_id' => "2", 'name' => 'Hihi', 'email' => 'hihi@gmail.com', 'password' => bcrypt('matkhau')],
+            ['role_id' => "3", 'name' => 'Haha', 'email' => 'haha@gmail.com', 'password' => bcrypt('matkhau')],
+            ['role_id' => "3", 'name' => 'Kaka', 'email' => 'Kaka@gmail.com', 'password' => bcrypt('matkhau')]
+
         ]);
     }
 }
